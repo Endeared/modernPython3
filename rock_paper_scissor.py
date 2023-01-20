0a7c3570927ae9ea90125a77001ad1b462f38ef2
Ross <rosshamey@gmail.com>
2023-01-20 10:39:01 -0500

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -2,7 +2,12 @@ print("...rock...")
 print("...paper...")
 print("...scissors...")
 
+count = 15
+
 player1 = input("(enter Player 1's choice): ")
+while (count < 15):
+    print("***NO CHEATING***")
+    count += 1
 player2 = input("(enter Player 2's choice): ")
 
 print("SHOOT!")
