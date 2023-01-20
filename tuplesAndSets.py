cf201d5bdc16da5059fc5becabfdaca3816c5778
Ross <rosshamey@gmail.com>
2023-01-20 17:55:59 -0500

    Create tuplesAndSets.py

---
+++ tuplesAndSets.py
@@ -0,0 +1,13 @@
+numbers = (1,2,3,4)
+value = (1)
+values = [10,20,30]
+static_values = tuple(values)
+
+stuff = [1,3,1,5,2,5,1,2,5]
+unique_stuff = set(stuff)
+
+
+print(numbers)
+print(value)
+print(static_values)
+print(unique_stuff)
