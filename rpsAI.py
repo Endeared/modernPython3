0bd43b4e3006f2d1be91df8bcb6b4205e2084f9b
Ross <rosshamey@gmail.com>
2023-01-20 10:52:13 -0500

    Update rpsAI.py

--- rpsAI.py
+++ rpsAI.py
@@ -8,25 +8,25 @@ print("...scissors...")
 count = 0
 
 player1 = input("(Enter your choice): ")
-randomIndex = random.randint(0, len(computerChoices - 1))
+randomIndex = random.randint(0, (len(computerChoices) - 1))
 computer = computerChoices[randomIndex]
 print("The computer plays: " + computer)
 
 print("SHOOT!")
 
 if (player1.lower() == computer.lower()):
-    print("TIE")
+    print("TIE!")
 elif (player1.lower() == 'scissors' and computer.lower() == 'paper'):
-    print("PLAYER 1 WINS")
+    print("YOU WIN!")
 elif (player1.lower() == 'scissors' and computer.lower() == 'rock'):
-    print("PLAYER 2 WINS")
+    print("COMPUTER WINS!")
 elif (player1.lower() == 'paper' and computer.lower() == 'rock'):
-    print("PLAYER 1 WINS")
+    print("YOU WIN!")
 elif (player1.lower() == 'paper' and computer.lower() == 'scissors'):
-    print("PLAYER 2 WINS")
+    print("COMPUTER WINS!")
 elif (player1.lower() == 'rock' and computer.lower() == 'scissors'):
-    print("PLAYER 1 WINS")
+    print("YOU WIN!")
 elif (player1.lower() == 'rock' and computer.lower() == 'paper'):
-    print("PLAYER 2 WINS")
+    print("COMPUTER WINS!")
 else:
     print("INVALID INPUT(S)!")
