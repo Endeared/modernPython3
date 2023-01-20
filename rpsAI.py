8b3b308b21d19fbcd5710ac02f7d6b6251b7cf81
Ross <rosshamey@gmail.com>
2023-01-20 10:48:27 -0500

    Update rpsAI.py

--- rpsAI.py
+++ rpsAI.py
@@ -8,23 +8,24 @@ print("...scissors...")
 count = 0
 
 player1 = input("(Enter your choice): ")
-computer = print("The computer plays: " + computerChoices[random.randint(0, len(computerChoices))])
+computer = computerChoices[random.randint(0, len(computerChoices))]
+computer = print("The computer plays: " + computer)
 
 print("SHOOT!")
 
-if (player1.lower() == player2.lower()):
+if (player1.lower() == computer.lower()):
     print("TIE")
-elif (player1.lower() == 'scissors' and player2.lower() == 'paper'):
+elif (player1.lower() == 'scissors' and computer.lower() == 'paper'):
     print("PLAYER 1 WINS")
-elif (player1.lower() == 'scissors' and player2.lower() == 'rock'):
+elif (player1.lower() == 'scissors' and computer.lower() == 'rock'):
     print("PLAYER 2 WINS")
-elif (player1.lower() == 'paper' and player2.lower() == 'rock'):
+elif (player1.lower() == 'paper' and computer.lower() == 'rock'):
     print("PLAYER 1 WINS")
-elif (player1.lower() == 'paper' and player2.lower() == 'scissors'):
+elif (player1.lower() == 'paper' and computer.lower() == 'scissors'):
     print("PLAYER 2 WINS")
-elif (player1.lower() == 'rock' and player2.lower() == 'scissors'):
+elif (player1.lower() == 'rock' and computer.lower() == 'scissors'):
     print("PLAYER 1 WINS")
-elif (player1.lower() == 'rock' and player2.lower() == 'paper'):
+elif (player1.lower() == 'rock' and computer.lower() == 'paper'):
     print("PLAYER 2 WINS")
 else:
     print("INVALID INPUT(S)!")
