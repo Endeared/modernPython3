5201be0eb68dd2d29159c2107658692561283e66
Ross <rosshamey@gmail.com>
2023-01-20 21:19:51 -0500

    Update functionsPractice.py

--- functionsPractice.py
+++ functionsPractice.py
@@ -1,4 +1,10 @@
 def product(num1, num2):
     return(num1 * num2)
 
-print(product(2, 4))
+def return_day(num):
+    days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    day = num - 1
+    if day >= 0 and day <= 6:
+        return(days[day])
+    else:
+        return None
