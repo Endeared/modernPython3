aa00fc2003e240c68f5c6f32ea6e793c07d6228a
Ross <rosshamey@gmail.com>
2023-01-21 12:10:57 -0500

    Create calculateExercise.py

---
+++ calculateExercise.py
@@ -0,0 +1,45 @@
+def calculate(**kwargs):
+
+    if kwargs['operation'] == 'add' and kwargs['make_float'] == False:
+        try:
+            return(kwargs['message'] + " " + str(int(kwargs['first'] + kwargs['second'])))
+        except KeyError:
+            return('The result is ' + str(int(kwargs['first'] + kwargs['second'])))
+    elif kwargs['operation'] == 'add' and kwargs['make_float'] == True:
+        try:
+            return(kwargs['message'] + " " + str(kwargs['first'] + kwargs['second']))
+        except KeyError:
+            return('The result is ' + str(kwargs['first'] + kwargs['second']))
+
+    if kwargs['operation'] == 'subtract' and kwargs['make_float'] == False:
+        try:
+            return(kwargs['message'] + " " + str(int(kwargs['first'] - kwargs['second'])))
+        except KeyError:
+            return('The result is ' + str(int(kwargs['first'] - kwargs['second'])))
+    elif kwargs['operation'] == 'subtract' and kwargs['make_float'] == True:
+        try:
+            return(kwargs['message'] + " " + str(kwargs['first'] - kwargs['second']))
+        except KeyError:
+            return('The result is ' + str(kwargs['first'] - kwargs['second']))
+
+    if kwargs['operation'] == 'multiply' and kwargs['make_float'] == False:
+        try:
+            return(kwargs['message'] + " " + str(int(kwargs['first'] * kwargs['second'])))
+        except KeyError:
+            return('The result is ' + str(int(kwargs['first'] * kwargs['second'])))
+    elif kwargs['operation'] == 'multiply' and kwargs['make_float'] == True:
+        try:
+            return(kwargs['message'] + " " + str(kwargs['first'] * kwargs['second']))
+        except KeyError:
+            return('The result is ' + str(kwargs['first'] * kwargs['second']))
+    
+    if kwargs['operation'] == 'divide' and kwargs['make_float'] == False:
+        try:
+            return(kwargs['message'] + " " + str(int(kwargs['first'] / kwargs['second'])))
+        except KeyError:
+            return('The result is ' + str(int(kwargs['first'] / kwargs['second'])))
+    elif kwargs['operation'] == 'divide' and kwargs['make_float'] == True:
+        try:
+            return(kwargs['message'] + " " + str(kwargs['first'] / kwargs['second']))
+        except KeyError:
+            return('The result is ' + str(kwargs['first'] / kwargs['second']))
