340a3ab132e627ecf8e83a8e4bfea31f1dfa6411
Ross <rosshamey@gmail.com>
2023-01-20 21:27:49 -0500

    Update functionsPractice.py

--- functionsPractice.py
+++ functionsPractice.py
@@ -14,3 +14,11 @@ def last_element(list):
         return(list[len(list) - 1])
     else:
         return None
+
+def number_compare(num1, num2):
+    if num1 > num2:
+        return("First is greater")
+    elif num2 > num1:
+        return("Second is greater")
+    else:
+        return("Numbers are equal")
