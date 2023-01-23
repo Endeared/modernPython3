47cacd20fbf2e839352e3de5f17074ded57833b3
Ross <114155576+Endeared@users.noreply.github.com>
2023-01-22 21:32:15 -0500

    Update debuggingPractice.py

--- debuggingPractice.py
+++ debuggingPractice.py
@@ -1,2 +1,7 @@
 def divide(num1, num2):
-    
+    try:
+        return num1 / num2
+    except TypeError:
+        return "Please provide two integers or floats"
+    except ZeroDivisionError:
+        return "Please do not divide by zero"
