ed006474d729ae21f51033afb5d1834bb836cb32
Ross <114155576+Endeared@users.noreply.github.com>
2023-01-23 16:44:45 -0500

    Create dad.py

---
+++ dad.py
@@ -0,0 +1,31 @@
+import requests
+from random import choice
+
+headers = {
+    "accept": "application/json"
+}
+params = {
+    "term": input("Let me tell you a joke! Give me a topic:\n")
+}
+response = requests.get(
+    "https://icanhazdadjoke.com/search",
+    headers=headers,
+    params=params
+)
+
+data = response.json()
+results = data["results"]
+jokeCount = data["jokeCount"]
+
+if jokeCount > 1:
+    print(
+        f"I've got {jokeCount} jokes about {params['term']}. Here's one:\n",
+        choice(results)['joke']
+    )
+elif jokeCount == 1:
+    print(
+        f"I've got one joke about {params['term']}. Here it is:\n",
+        results[0]['joke']
+    )
+else:
+    print(f"Sorry, I don't have any jokes about {params['term']}! Please try again.")
