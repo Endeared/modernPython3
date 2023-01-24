eb9e320b6129eac6ca2eee1a0c34e65784f55236
Ross <114155576+Endeared@users.noreply.github.com>
2023-01-23 20:32:47 -0500

    Update deckOfCards.py

--- deckOfCards.py
+++ deckOfCards.py
@@ -1,3 +1,5 @@
+from pprintpp import pprint
+
 class Card:
 
     def __init__(self, suit, value):
@@ -26,7 +28,19 @@ class Deck:
             self.cards.append(Card("Clubs", str(j)))
             self.cards.append(Card("Diamonds", str(j)))
             self.cards.append(Card("Spades", str(j)))
+        self.num = len(self.cards)
+        
+
+    def count(self):
+        return self.num
+
+    def __repr__(self):
+        return(f'Deck of {self.num} cards')
+
+    
 
 cards = Deck()
 
-print(cards.cards)
+pprint(cards.cards)
+pprint(repr(cards))
+pprint(cards.num)
