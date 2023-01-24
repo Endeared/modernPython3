dc86f26fca08a20f440eb482f2e4d1fb6c02740b
Ross <114155576+Endeared@users.noreply.github.com>
2023-01-23 19:38:54 -0500

    Update bankAccount.py

--- bankAccount.py
+++ bankAccount.py
@@ -1,4 +1,13 @@
 class BankAccount:
     
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, owner, balance=0.0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self.balance
