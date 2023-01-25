468f52978ba0c274c64764432ce71806f9c75543
Ross <rosshamey@gmail.com>
2023-01-25 11:48:18 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,1 +1,8 @@
+def get_data():
+    email_address = input('Enter Email: ')
+    password = input('Enter Password: ')
 
+    return email_address, password
+
+email, password = get_data()
+print(email + " ][ " + password)
