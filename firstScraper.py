3cb061c98742f3e32c2b9a8925e71a7017ad9084
Ross <rosshamey@gmail.com>
2023-01-26 13:32:11 -0500

    Create firstScraper.py

---
+++ firstScraper.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+response = requests.get("https://www.rithmschool.com/blog")
+soup = BeautifulSoup(response.text, "html.parser")
+articles = soup.find_all("article")
+print(articles)
+
+
+for article in articles:
+    a_tag = article.find("a")
+    title = a_tag.get_text()
+    url = a_tag['href']
+    date = article.find("time")['datetime']
+    print(title, url, date)
