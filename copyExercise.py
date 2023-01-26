19f5c0d936c9756076c16942b6a5eb272549e5d1
Ross <rosshamey@gmail.com>
2023-01-25 21:49:45 -0500

    Update copyExercise.py

--- copyExercise.py
+++ copyExercise.py
@@ -1,4 +1,5 @@
-def copy(file, newFile):
+def copy_and_reverse(file, newFile):
     with open(file, 'r') as firstFile, open(newFile, 'w+') as secondFile:
-        for line in firstFile:
-            secondFile.write(line)
+        data = firstFile.read()
+        dataReverse = data[::-1]
+        secondFile.write(dataReverse)
