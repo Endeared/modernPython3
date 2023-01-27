59ec53f58f3ccba148b05e8ac481282b700c58ed
Ross <rosshamey@gmail.com>
2023-01-27 10:15:12 -0500

    Create regexDate.py

---
+++ regexDate.py
@@ -0,0 +1,11 @@
+import re
+
+def parse_bytes(check):
+    checkFor = re.compile(r'^(\d\d)[,/.](\d\d)[,/.](\d{4})$')
+    found = checkFor.search(check)
+    if found:
+        return {
+            "d": found.group(1),
+            "m": found.group(2),
+            "y": found.group(3)
+        }
