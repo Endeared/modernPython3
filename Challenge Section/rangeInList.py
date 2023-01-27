293d734d24e7b0d29730b01752e949c330632412
Ross <rosshamey@gmail.com>
2023-01-27 18:36:34 -0500

    Create rangeInList.py

---
+++ Challenge Section/rangeInList.py
@@ -0,0 +1,12 @@
+def range_in_list(values, start=0, end=0):
+    sum = 0
+    if end == 0:
+        end = len(values) - 1
+    elif values == []:
+        return 0
+
+    while start <= end and start <= len(values) - 1:
+        sum += values[start]
+        start += 1
+    
+    return sum
