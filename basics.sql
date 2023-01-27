bb1aff1a162056dad1b8798e646283df5786dd6e
Ross <rosshamey@gmail.com>
2023-01-27 10:38:29 -0500

    first sqlite3 db

---
+++ basics.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE dogs (
+--     name TEXT,
+--     breed TEXT,
+--     age INTEGER
+-- );
+
+INSERT INTO dogs (name, age, breed) VALUES ("Toby", 11, "Shepherd-Beagle");
+INSERT INTO dogs (name, age, breed) VALUES ("Zipper", 14, "Golden Retriever");
+INSERT INTO dogs (name, age, breed) VALUES ("Shadow", 16, "Labrador");
+INSERT INTO dogs (name, age, breed) VALUES ("Elvis", 11, "Yorkie");
