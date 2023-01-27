6503a385993bde2f0b413527a61703aa91ab3d71
Ross <rosshamey@gmail.com>
2023-01-27 10:02:14 -0500

    Update regexTime.py

--- regexTime.py
+++ regexTime.py
@@ -1,7 +1,7 @@
 import re
 
 def is_valid_time(check):
-    checkFor = re.compile(f'^\d\d?:\d\d$')
+    checkFor = re.compile(r'^\d\d?:\d\d$')
     found = checkFor.search(check)
     if found:
         return True
