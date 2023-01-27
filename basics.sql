bbca9528d2678a56b6ebdf82ec81f6eacb4e8a5e
Ross <rosshamey@gmail.com>
2023-01-27 10:41:17 -0500

    Update basics.sql

--- basics.sql
+++ basics.sql
@@ -8,3 +8,8 @@ INSERT INTO dogs (name, age, breed) VALUES ("Toby", 11, "Shepherd-Beagle");
 INSERT INTO dogs (name, age, breed) VALUES ("Zipper", 14, "Golden Retriever");
 INSERT INTO dogs (name, age, breed) VALUES ("Shadow", 16, "Labrador");
 INSERT INTO dogs (name, age, breed) VALUES ("Elvis", 11, "Yorkie");
+
+
+CREATE TABLE temp_table as SELECT DISTINCT * FROM dogs;
+DELETE FROM dogs;
+INSERT INTO dogs SELECT * FROM temp_table
