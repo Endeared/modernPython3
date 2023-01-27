cdd4841cb6d3d9e1d9ed14558d504fe75ae025f5
Ross <rosshamey@gmail.com>
2023-01-27 10:48:41 -0500

    updated friends.py

--- friends.py
+++ friends.py
@@ -2,7 +2,19 @@ import sqlite3
 
 connection = sqlite3.connect('my_friends.db')
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER)")
+# cursor.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER)")
+
+friends = [
+    ("Hannah",'Clemmons', 23),
+    ('Derek','Smith', 23),
+    ('Morgan','Schmit', 23)
+]
+
+# insert_query = '''INSERT INTO friends VALUES ('Hannah', 'Clemmons', 23)'''
+
+for friend in friends:
+    cursor.execute("INSERT INTO friends VALUES (?,?,?)", friend)
+    print("INSERTED!")
 
 connection.commit()
 connection.close()
